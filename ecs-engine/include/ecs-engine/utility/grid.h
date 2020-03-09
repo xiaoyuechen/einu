73ebb24d2ed66e1154fe136f91633176ef9d7c5b
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2020-03-10 00:14:39 +0100

    Runtime grid

--- ecs-engine/include/ecs-engine/utility/grid.h
+++ ecs-engine/include/ecs-engine/utility/grid.h
@@ -1,76 +1,89 @@
 #ifndef GRID_H_
 #define GRID_H_
 
-#include <array>
+#include <vector>
+
+#include "ecs-engine/math/glm.h"
 
 namespace ecs {
 
-template <typename Cell, std::size_t size_x, std::size_t size_y>
+template <typename T>
 class Grid {
  public:
-  constexpr const Cell* operator[](std::size_t count_y) const noexcept;
-  constexpr Cell* operator[](std::size_t count_y) noexcept;
-  constexpr const Cell* begin() const noexcept;
-  constexpr Cell* begin() noexcept;
-  constexpr const Cell* end() const noexcept;
-  constexpr Cell* end() noexcept;
+  Grid(glm::uvec2 size);
+  const T* operator[](std::size_t y) const noexcept;
+  T* operator[](std::size_t y) noexcept;
+  const T* begin() const noexcept;
+  T* begin() noexcept;
+  const T* end() const noexcept;
+  T* end() noexcept;
+  void GetCells(std::size_t x, std::size_t y, std::size_t x_count,
+                std::size_t y_count, const T** dest) const;
 
-
-  static constexpr std::size_t GetSizeX() noexcept;
-  static constexpr std::size_t GetSizeY() noexcept;
-  static constexpr std::size_t GetTotalCellCount() noexcept;
+  glm::uvec2 GetSize() const noexcept;
+  std::size_t GetTotalCellCount() const noexcept;
 
  private:
-  std::array<Cell, GetTotalCellCount()> grid_;
+  glm::uvec2 size_;
+  std::vector<T> grid_;
 };
 
 //////////////////////////////////////////////////////////////////////////
 
-template <typename Cell, std::size_t size_x, std::size_t size_y>
-inline constexpr const Cell* Grid<Cell, size_x, size_y>::operator[](std::size_t count_y) const
-    noexcept {
-  return grid_.data() + count_y * size_x;
+template <typename T>
+inline Grid<T>::Grid(glm::uvec2 size) {
+  size_ = size;
+  grid_.resize(GetTotalCellCount());
 }
 
-template <typename Cell, std::size_t size_x, std::size_t size_y>
-inline constexpr Cell* Grid<Cell, size_x, size_y>::operator[](
-    std::size_t count_y) noexcept {
-  return const_cast<Cell*>(static_cast<const Grid&>(*this)[count_y]);
+template <typename T>
+inline const T* Grid<T>::operator[](std::size_t y) const noexcept {
+  return grid_.data() + y * size_.x;
 }
 
-template <typename Cell, std::size_t size_x, std::size_t size_y>
-inline constexpr const Cell* Grid<Cell, size_x, size_y>::begin() const noexcept {
+template <typename T>
+inline T* Grid<T>::operator[](std::size_t y) noexcept {
+  return const_cast<T*>(static_cast<const Grid&>(*this)[y]);
+}
+
+template <typename T>
+inline const T* Grid<T>::begin() const noexcept {
   return grid_.data();
 }
 
-template <typename Cell, std::size_t size_x, std::size_t size_y>
-inline constexpr Cell* Grid<Cell, size_x, size_y>::begin() noexcept {
-  return const_cast<Cell*>(static_cast<const Grid&>(*this).begin());
+template <typename T>
+inline T* Grid<T>::begin() noexcept {
+  return const_cast<T*>(static_cast<const Grid&>(*this).begin());
 }
 
-template <typename Cell, std::size_t size_x, std::size_t size_y>
-inline constexpr const Cell* Grid<Cell, size_x, size_y>::end() const noexcept {
+template <typename T>
+inline const T* Grid<T>::end() const noexcept {
   return grid_.data() + GetTotalCellCount();
 }
 
-template <typename Cell, std::size_t size_x, std::size_t size_y>
-inline constexpr Cell* Grid<Cell, size_x, size_y>::end() noexcept {
-  return const_cast<Cell*>(static_cast<const Grid&>(*this).end());
+template <typename T>
+inline T* Grid<T>::end() noexcept {
+  return const_cast<T*>(static_cast<const Grid&>(*this).end());
 }
 
-template <typename Cell, std::size_t size_x, std::size_t size_y>
-inline constexpr std::size_t Grid<Cell, size_x, size_y>::GetSizeX() noexcept {
-  return size_x;
+template <typename T>
+inline void Grid<T>::GetCells(std::size_t x, std::size_t y, std::size_t x_count,
+                              std::size_t y_count, const T** dest) const {
+  for (std::size_t y_index = 0; y_index != y_count; ++y_index) {
+    for (std::size_t x_index = 0; x_index != x_count; ++x_index) {
+      dest[x_index + y_index * x_count] = &(*this)[x + x_index][y + y_index];
+    }
+  }
 }
 
-template <typename Cell, std::size_t size_x, std::size_t size_y>
-inline constexpr std::size_t Grid<Cell, size_x, size_y>::GetSizeY() noexcept {
-  return size_y;
+template <typename T>
+inline glm::uvec2 Grid<T>::GetSize() const noexcept {
+  return size_;
 }
 
-template <typename Cell, std::size_t size_x, std::size_t size_y>
-inline constexpr std::size_t Grid<Cell, size_x, size_y>::GetTotalCellCount() noexcept {
-  return size_x * size_y;
+template <typename T>
+inline std::size_t Grid<T>::GetTotalCellCount() const noexcept {
+  return static_cast<std::size_t>(size_.x) * size_.y;
 }
 
 }  // namespace ecs
