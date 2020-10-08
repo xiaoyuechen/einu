e36b910bd80e8673830accdb87d34ef5af45456e
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2020-10-09 01:30:58 +0200

    Remove allocator template parameter from fixed pool

--- einu-engine/einu-util/include/einu-util/object_pool.h
+++ einu-engine/einu-util/include/einu-util/object_pool.h
@@ -9,23 +9,19 @@
 namespace einu {
 namespace util {
 
-template <typename T, typename Allocator = std::allocator<T>,
-          typename BitArrayAllocator = std::allocator<bool>>
+template <typename T>
 class FixedPool {
  public:
   using size_type = std::size_t;
   using value_type = T;
 
-  explicit FixedPool(size_type count, const Allocator& alloc = Allocator{},
-                     const BitArrayAllocator& ba_alloc = BitArrayAllocator{})
-      : object_arr_(count, alloc)
-      , bit_arr_(count, true, ba_alloc) {}
+  explicit FixedPool(size_type count)
+      : object_arr_(count)
+      , bit_arr_(count, true) {}
 
-  FixedPool(size_type count, const T& value,
-            const Allocator& alloc = Allocator{},
-            const BitArrayAllocator& ba_alloc = BitArrayAllocator{})
-      : object_arr_(count, value, alloc)
-      , bit_arr_(count, true, ba_alloc) {}
+  FixedPool(size_type count, const T& value)
+      : object_arr_(count, value)
+      , bit_arr_(count, true) {}
 
   FixedPool(const FixedPool&) = delete;
   FixedPool& operator=(const FixedPool&) = delete;
@@ -49,13 +45,12 @@ class FixedPool {
   }
 
  private:
-  std::vector<T, Allocator> object_arr_;
-  std::vector<bool, BitArrayAllocator> bit_arr_;
+  std::vector<T> object_arr_;
+  std::vector<bool> bit_arr_;
 };
 
-template <typename T, typename Allocator, typename BitArrayAllocator>
-bool AllAcquired(
-    const FixedPool<T, Allocator, BitArrayAllocator>& pool) noexcept {
+template <typename T>
+bool AllAcquired(const FixedPool<T>& pool) noexcept {
   return pool.FreePos() == pool.Size();
 }
 
