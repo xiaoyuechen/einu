23a8ba2bffdd49744258766180eb067116050be0
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2020-10-09 03:21:41 +0200

    Implement and test dynamic pool

--- einu-engine/einu-util/include/einu-util/object_pool.h
+++ einu-engine/einu-util/include/einu-util/object_pool.h
@@ -2,6 +2,8 @@
 
 #include <cassert>
 #include <functional>
+#include <numeric>
+#include <optional>
 #include <vector>
 
 #include "einu-util/bit.h"
@@ -25,10 +27,17 @@ class FixedPool {
 
   FixedPool(const FixedPool&) = delete;
   FixedPool& operator=(const FixedPool&) = delete;
+  FixedPool(FixedPool&&) = default;
+  FixedPool& operator=(FixedPool&&) = default;
 
   size_type Size() const noexcept { return object_arr_.size(); }
   size_type FreePos() const noexcept {
     return FindFirstSet(bit_arr_.begin(), bit_arr_.end());
+  }
+
+  bool Has(const T& obj) const noexcept {
+    auto idx = &obj - object_arr_.data();
+    return 0 <= idx && (unsigned)idx < Size();
   }
 
   [[nodiscard]] T& Acquire() noexcept {
@@ -36,6 +45,12 @@ class FixedPool {
     assert(free_pos != bit_arr_.size() && "no object available");
     bit_arr_[free_pos] = false;
     return object_arr_[free_pos];
+  }
+
+  [[nodiscard]] T& Acquire(size_type pos_hint) noexcept {
+    assert(bit_arr_[pos_hint] && "object at pos is not available");
+    bit_arr_[pos_hint] = false;
+    return object_arr_[pos_hint];
   }
 
   void Release(const T& obj) noexcept {
@@ -53,6 +68,78 @@ template <typename T>
 bool AllAcquired(const FixedPool<T>& pool) noexcept {
   return pool.FreePos() == pool.Size();
 }
+
+/**
+ * This policy will double the pool size when growing is required
+ * Also constructs objects with default constructor
+ */
+template <typename T>
+class DefaultGrowPolicy {
+ public:
+  std::size_t GetGrowSize(std::size_t old_size) const noexcept {
+    return old_size;
+  }
+  const T& GetValue() const noexcept { return value_; }
+
+ private:
+  T value_{};
+};
+
+template <typename T, typename GrowPolicy = DefaultGrowPolicy<T>>
+class DynamicPool : public GrowPolicy {
+ public:
+  using size_type = std::size_t;
+  using value_type = T;
+
+  explicit DynamicPool(size_type count) {
+    pools_.emplace_back(count, GrowPolicy::GetValue());
+  }
+
+  [[nodiscard]] T& Acquire() {
+    if (PoolsAllAcquired()) {
+      pools_.emplace_back(GrowPolicy::GetGrowSize(Size()),
+                          GrowPolicy::GetValue());
+    }
+
+    for (auto& pool : pools_) {
+      auto free_pos = pool.FreePos();
+      if (free_pos != pool.Size()) {
+        return pool.Acquire(free_pos);
+      }
+    }
+    assert(false);
+    return pools_[0].Acquire();
+  }
+
+  void Release(const T& obj) noexcept {
+    for (auto& pool : pools_) {
+      if (pool.Has(obj)) {
+        pool.Release(obj);
+        return;
+      }
+    }
+    assert(false && "object does not belong to this pool");
+  }
+
+  size_type Size() const noexcept {
+    return std::accumulate(
+        pools_.begin(), pools_.end(), size_type{0},
+        [](auto acc, const auto& pool) { return acc + pool.Size(); });
+  }
+
+ private:
+  using FixedPool = FixedPool<T>;
+  using PoolList = std::vector<FixedPool>;
+
+  bool PoolsAllAcquired() const noexcept {
+    for (const auto& pool : pools_) {
+      if (!AllAcquired(pool)) return false;
+    }
+    return true;
+  }
+
+  PoolList pools_;
+};
 
 }  // namespace util
 }  // namespace einu
--- tests/einu-util-tests/object_pool_test.cc
+++ tests/einu-util-tests/object_pool_test.cc
@@ -4,7 +4,7 @@
 namespace einu {
 namespace util {
 
-struct FixedPoolTest : testing::Test {
+struct FixedPoolTest : public testing::Test {
   static constexpr std::size_t kSize = 100;
   FixedPool<int> pool{kSize};
 };
@@ -38,6 +38,59 @@ TEST_F(FixedPoolTest, free_pos_move_right_after_acquire) {
     pre_free_pos = free_pos;
   });
   EXPECT_TRUE(AllAcquired(pool));
+}
+
+struct DynamicPoolTest : public testing::Test {
+  static constexpr std::size_t kSize = 100;
+  DynamicPool<int> pool{kSize};
+};
+
+TEST_F(DynamicPoolTest, pool_size_can_grow) {
+  EXPECT_EQ(pool.Size(), kSize);
+  auto new_size = pool.GetGrowSize(pool.Size()) + pool.Size();
+  for (std::size_t i = 0; i != kSize + 1; ++i) {
+    [[maybe_unused]] auto& r = pool.Acquire();
+  }
+  EXPECT_EQ(pool.Size(), new_size);
+}
+
+TEST_F(DynamicPoolTest, pool_only_grows_when_full) {
+  EXPECT_EQ(pool.Size(), kSize);
+  for (std::size_t i = 0; i != kSize; ++i) {
+    [[maybe_unused]] auto& r = pool.Acquire();
+  }
+  EXPECT_EQ(pool.Size(), kSize);
+  auto new_size = pool.GetGrowSize(pool.Size()) + pool.Size();
+  [[maybe_unused]] auto& r = pool.Acquire();
+  EXPECT_EQ(pool.Size(), new_size);
+}
+
+TEST_F(DynamicPoolTest, release_would_make_more_space_available) {
+  EXPECT_EQ(pool.Size(), kSize);
+  auto acquired = std::vector<decltype(pool)::value_type*>{kSize + 1};
+  auto new_size = pool.GetGrowSize(pool.Size()) + pool.Size();
+  std::for_each(acquired.begin(), acquired.end(),
+                [&](auto&& v) { v = &pool.Acquire(); });
+  EXPECT_EQ(pool.Size(), new_size);
+  std::for_each(acquired.rbegin(), acquired.rend(),
+                [&](auto&& v) { pool.Release(*v); });
+
+  acquired.resize(pool.Size());
+  std::for_each(acquired.begin(), acquired.end(),
+                [&](auto&& v) { v = &pool.Acquire(); });
+  EXPECT_EQ(pool.Size(), new_size);
+}
+
+TEST_F(DynamicPoolTest, release_would_not_change_size) {
+  EXPECT_EQ(pool.Size(), kSize);
+  auto acquired = std::vector<decltype(pool)::value_type*>{kSize + 1};
+  auto new_size = pool.GetGrowSize(pool.Size()) + pool.Size();
+  std::for_each(acquired.begin(), acquired.end(),
+                [&](auto&& v) { v = &pool.Acquire(); });
+  EXPECT_EQ(pool.Size(), new_size);
+  std::for_each(acquired.rbegin(), acquired.rbegin(),
+                [&](auto&& v) { pool.Release(*v); });
+  EXPECT_EQ(pool.Size(), new_size);
 }
 
 }  // namespace util
