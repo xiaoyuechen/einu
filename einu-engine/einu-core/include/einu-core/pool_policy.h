9a2485e500af1da0ca421e203815f10167b41941
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2020-10-11 14:44:17 +0200

    Add template specializaton to PoolPolicy

--- einu-engine/einu-core/include/einu-core/pool_policy.h
+++ einu-engine/einu-core/include/einu-core/pool_policy.h
@@ -9,20 +9,35 @@ constexpr std::size_t DefaultComponentPoolGrowFunc(
   return pool_size == 0 ? 1 : 2 * pool_size;
 }
 
+template <typename... T>
+struct PoolPolicy;
+
 template <typename T>
-struct PoolPolicy {
+struct PoolPolicy<T> {
   using size_type = std::size_t;
   using value_type = T;
 
-  constexpr PoolPolicy(
-      size_type init_size, const value_type& value = value_type{},
-      GrowthFunc growth_func = DefaultComponentPoolGrowFunc) noexcept
+  PoolPolicy(size_type init_size, const value_type& value = value_type{},
+             GrowthFunc growth_func = DefaultComponentPoolGrowFunc) noexcept
       : init_size(init_size)
       , value(value)
       , growth_func(growth_func) {}
 
   size_type init_size;
   value_type value;
+  GrowthFunc growth_func;
+};
+
+template <>
+struct PoolPolicy<> {
+  using size_type = std::size_t;
+
+  PoolPolicy(size_type init_size,
+             GrowthFunc growth_func = DefaultComponentPoolGrowFunc) noexcept
+      : init_size(init_size)
+      , growth_func(growth_func) {}
+
+  size_type init_size;
   GrowthFunc growth_func;
 };
 
