b98939c17ea100ff3c13df78b3788650a7e551c7
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2020-10-11 19:28:13 +0200

    Add tests for EntityPool

--- einu-engine/einu-core/include/einu-core/internal/entity_pool.h
+++ einu-engine/einu-core/include/einu-core/internal/entity_pool.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <einu-util/object_pool.h>
+
+#include <atomic>
+
 #include "einu-core/i_entity_pool.h"
 #include "einu-core/internal/entity.h"
 #include "einu-core/internal/xnent_mask.h"
@@ -7,18 +11,49 @@
 namespace einu {
 namespace internal {
 
+class SimpleEIDManager {
+ public:
+  EID Acquire() noexcept { return available_++; }
+  void Release() noexcept {}
+
+ private:
+  std::atomic_uint64_t available_ = 0;
+};
+
+template <std::size_t max_comp, typename EIDManager>
 class EntityPool final : public IEntityPool {
  public:
+  explicit EntityPool(EIDManager& eid_manager)
+      : eid_manager_(eid_manager) {}
+
  private:
-  void SetPolicyImpl(const Policy& policy) override {}
+  void SetPolicyImpl(const Policy& policy) override {
+    pool_.SetGrowth(policy.growth_func);
+    pool_.GrowExtra(policy.init_size);
+  }
 
   IEntity& AcquireImpl() override {
+    auto eid = eid_manager_.Acquire();
+    auto&& [ett, mask, table] = pool_.Acquire();
+    mask.reset();
+    table.fill(nullptr);
+    ett = Entity(eid, mask, table);
+    return ett;
+  }
+
+  void ReleaseImpl(const IEntity& ett) noexcept override {
     throw std::logic_error("The method or operation is not implemented.");
   }
 
-  void ReleaseImpl(const IEntity& ett) override {
-    throw std::logic_error("The method or operation is not implemented.");
-  }
+  size_type SizeImpl() const noexcept override { return pool_.Size(); }
+
+  using Entity = Entity<max_comp>;
+  using ComponentMask = StaticXnentMask<max_comp>;
+  using ComponentTable = StaticComponentTable<max_comp>;
+  using Pool = util::DynamicPool<Entity, ComponentMask, ComponentTable>;
+
+  EIDManager& eid_manager_;
+  Pool pool_;
 };
 
 }  // namespace internal
--- tests/einu-core-tests/entity_test.cc
+++ tests/einu-core-tests/entity_test.cc
@@ -1,8 +1,9 @@
-#include <einu-core/einu_core.h>
 #include <einu-core/internal/entity.h>
 #include <gtest/gtest.h>
 
 namespace einu {
+
+// TODO(Xiaoyue Chen): Rewrite entity test
 
 struct C0 : Xnent {};
 struct C1 : Xnent {};
@@ -10,16 +11,12 @@ struct C2 : Xnent {};
 struct C3 : Xnent {};
 
 using CL = XnentList<C0, C1, C2, C3>;
-using NL = NeedList<CL, XnentList<>>;
-using TestEnginePolicy = EnginePolicy<NL>;
-using TestEngine = EinuEngine<TestEnginePolicy>;
 using Entity = internal::Entity<tmp::Size<ToTypeList<CL>::Type>::value>;
 
 struct EntityTest : public testing::Test {
   EntityTest()
       : entity(0, mask, table) {}
 
-  TestEngine engine;
   C0 c0;
   C1 c1;
   C2 c2;
@@ -39,13 +36,13 @@ struct EntityTest : public testing::Test {
 //  EXPECT_TRUE(entity.HasComponents(XnentList<C0, C1, C3>{}));
 //}
 
-TEST_F(EntityTest, can_remove_component) {
-  entity.AddComponent(c0);
-  EXPECT_TRUE(entity.HasComponents(XnentList<C0>{}));
-  auto& lc0 = entity.RemoveComponent<C0>();
-  EXPECT_FALSE(entity.HasComponents(XnentList<C0>{}));
-  EXPECT_EQ(&lc0, &c0);
-}
+// TEST_F(EntityTest, can_remove_component) {
+//  entity.AddComponent(c0);
+//  EXPECT_TRUE(entity.HasComponents(XnentList<C0>{}));
+//  auto& lc0 = entity.RemoveComponent<C0>();
+//  EXPECT_FALSE(entity.HasComponents(XnentList<C0>{}));
+//  EXPECT_EQ(&lc0, &c0);
+//}
 
 TEST_F(EntityTest, can_get_id) {
   EXPECT_EQ(entity.GetID(), 0);
