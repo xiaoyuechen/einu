8ce40a67fa45a6335dac50ce59ad4f5001b52e47
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2020-10-11 16:45:34 +0200

    Make Entity default constructable

--- einu-engine/einu-core/include/einu-core/internal/entity.h
+++ einu-engine/einu-core/include/einu-core/internal/entity.h
@@ -18,43 +18,44 @@ class Entity final : public IEntity {
   using StaticXnentMask = StaticXnentMask<max_comp>;
   using ComponentTable = StaticComponentTable<max_comp>;
 
+  Entity() = default;
   Entity(EID eid, StaticXnentMask& mask, ComponentTable& table) noexcept
       : eid_{eid}
-      , mask_{mask}
-      , table_{table} {}
+      , mask_{&mask}
+      , table_{&table} {}
 
  private:
   EID GetIDImpl() const noexcept override { return eid_; }
 
   bool HasComponentsImpl(const DynamicXnentMask& mask) const noexcept override {
-    return (mask & mask_) == mask;
+    return (mask & *mask_) == mask;
   }
 
   const Xnent& GetComponentImpl(XnentTypeID idx) const noexcept override {
-    return *table_[idx];
+    return *(*table_)[idx];
   }
 
   Xnent& GetComponentImpl(XnentTypeID idx) noexcept override {
-    return *table_[idx];
+    return *(*table_)[idx];
   }
 
   void AddComponentImpl(XnentTypeID idx, Xnent& comp) override {
-    assert(!mask_[idx] && "already have component");
-    table_[idx] = &comp;
-    mask_[idx] = true;
+    assert(!(*mask_)[idx] && "already have component");
+    (*table_)[idx] = &comp;
+    (*mask_)[idx] = true;
   }
 
   Xnent& RemoveComponentImpl(XnentTypeID idx) noexcept override {
-    assert(mask_[idx] && "do not have component");
-    mask_[idx] = false;
-    auto& tmp = *table_[idx];
-    table_[idx] = nullptr;
+    assert((*mask_)[idx] && "do not have component");
+    (*mask_)[idx] = false;
+    auto& tmp = *(*table_)[idx];
+    (*table_)[idx] = nullptr;
     return tmp;
   }
 
   EID eid_ = ~EID{0};
-  StaticXnentMask& mask_;
-  ComponentTable& table_;
+  StaticXnentMask* mask_ = nullptr;
+  ComponentTable* table_ = nullptr;
 };
 
 }  // namespace internal
