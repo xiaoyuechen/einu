105dc66ffb33e41215edb59a1ca9af6bc3dc99e7
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2020-10-24 16:01:12 +0200

    Format source

--- einu-engine/core/tests/need_list_test.cc
+++ einu-engine/core/tests/need_list_test.cc
@@ -1,7 +1,7 @@
 // Copyright (C) 2020  Xiaoyue Chen
 //
-// This file is part of EINU Engine. 
-// See <https://github.com/xiaoyuechen/einu.git>. 
+// This file is part of EINU Engine.
+// See <https://github.com/xiaoyuechen/einu.git>.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
