09005ac0c55d7bc8c1573c968dac171eca356c64
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2020-10-25 15:28:24 +0100

    Fix engine core util/enum

--- einu-engine/core/include/einu-engine/core/util/enum.h
+++ einu-engine/core/include/einu-engine/core/util/enum.h
@@ -82,8 +82,8 @@ template <typename Enum>
 typename std::enable_if<EnableBitMaskOperators<Enum>::enable, Enum&>::type
 operator^=(Enum& lhs, Enum rhs) {
   using underlying = typename std::underlying_type<Enum>::type;
-  lhs = static_cast<Enum>(
-      static_cast<underlying>(lhs) static_cast<underlying>(rhs));
+  lhs = static_cast<Enum>(static_cast<underlying>(lhs) ^
+                          static_cast<underlying>(rhs));
   return lhs;
 }
 
